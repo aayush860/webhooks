32dedf39f0bf930e89a98e2954fcd332baf28fc1
Aayush Bhargava <aayush.bhrgv@gmail.com>
2020-08-04 01:56:53 +0530

    message

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn weebhooks.wsgi
---
+++ webhooks/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render,redirect
+from django.http import HttpResponse
+from database.models import impression_store
+from django.views.decorators.csrf import csrf_exempt
+
+@csrf_exempt
+def upload(request):
+    if request.method=='POST':
+        time = request.POST['time']
+        impression = request.POST['impression']
+        print (time,impression)
+        imp = impression_store.objects.create(impression=impression, time=time)
+        imp.save()
+        Data = impression_store.objects.all()
+        return render(request, 'display.html', {'Data':Data})
+
+def display(request):
+    if request.method=='GET':
+        Data = impression_store.objects.all()
+        return render(request, 'display.html', {'Data':Data})
+    
