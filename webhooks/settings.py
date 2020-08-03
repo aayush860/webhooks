7521e053fd9082b0591b7811fae2fd7d73173ce0
Aayush Bhargava <aayush.bhrgv@gmail.com>
2020-08-04 02:05:07 +0530

    Update settings.py

--- webhooks/settings.py
+++ webhooks/settings.py
@@ -127,9 +127,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIRS=[os.path.join(BASE_DIR,'templates/static/')]
-STATIC_ROOT = os.path.join(BASE_DIR,'templates/assets/')
 SITE_ID=1
-
+DISABLE_COLLECTSTATIC=1
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
